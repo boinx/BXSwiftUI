867bc99a6ab5d088fad3ddf63a7bb41d15aa4723
Peter Baumgartner <peter@boinx.com>
2020-01-25 14:47:29 +0100

    Added more to TypeCheckable

--- BXSwiftUI/TypeCheckable.swift
+++ BXSwiftUI/TypeCheckable.swift
@@ -25,10 +25,25 @@ extension TypeCheckable
     {
 		return Self.defaultValue is String
 	}
+	
+    static var isURL:Bool
+    {
+		return Self.defaultValue is URL
+	}
 
     static var isDouble:Bool
     {
 		return Self.defaultValue is Double
+	}
+
+    static var isFloat:Bool
+    {
+		return Self.defaultValue is Float
+	}
+
+    static var isCGFloat:Bool
+    {
+		return Self.defaultValue is CGFloat
 	}
 
     static var isInt:Bool
@@ -51,7 +66,22 @@ extension String : TypeCheckable
 	public static var defaultValue:Self { "" }
 }
 
+extension URL : TypeCheckable
+{
+	public static var defaultValue:Self { URL(string:"https://www.boinx.com")! }
+}
+
 extension Double : TypeCheckable
+{
+	public static var defaultValue:Self { 0.0 }
+}
+
+extension Float : TypeCheckable
+{
+	public static var defaultValue:Self { 0.0 }
+}
+
+extension CGFloat : TypeCheckable
 {
 	public static var defaultValue:Self { 0.0 }
 }
