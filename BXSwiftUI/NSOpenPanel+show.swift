bebb23b50409911e55b42d4c5e33279549936cdb
Peter Baumgartner <peter@boinx.com>
2020-01-30 19:28:11 +0100

    Added convenience functions for NSOpenPanel and NSSavePanel

---
+++ BXSwiftUI/NSOpenPanel+show.swift
@@ -0,0 +1,64 @@
+//**********************************************************************************************************************
+//
+//  NSOpenPanel+show.swift
+//	Convenience function to show an NSOpenPanel
+//  Copyright ©2020 Peter Baumgartner. All rights reserved.
+//
+//**********************************************************************************************************************
+
+
+import SwiftUI
+import AppKit
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
+public extension NSOpenPanel
+{
+	class func show(title:String? = nil, message:String? = nil, buttonLabel:String? = nil, allowedFileTypes:[String]? = nil, canChooseFiles:Bool = true, canChooseDirectories:Bool = false, allowsMultipleSelection:Bool = false, handler:([URL])->Void)
+	{
+		let panel = NSOpenPanel()
+		
+		panel.canCreateDirectories = true
+		panel.canChooseFiles = canChooseFiles
+		panel.canChooseDirectories = canChooseDirectories
+		panel.allowsMultipleSelection = allowsMultipleSelection
+
+		if let title = title
+		{
+			panel.title = title
+		}
+		
+		if let message = message
+		{
+			panel.message = message
+		}
+		
+		if let buttonLabel = buttonLabel
+		{
+			panel.prompt = buttonLabel
+		}
+
+		if let allowedFileTypes = allowedFileTypes
+		{
+			panel.allowedFileTypes = allowedFileTypes
+		}
+
+		let button = panel.runModal()
+		
+		if button == NSApplication.ModalResponse.OK
+		{
+			handler(panel.urls)
+		}
+		else
+		{
+			handler([])
+		}
+	}
+}
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
---
+++ BXSwiftUI/NSSavePanel+show.swift
@@ -0,0 +1,61 @@
+//**********************************************************************************************************************
+//
+//  NSSavePanel+show.swift
+//	Convenience function to show an NSSavePanel
+//  Copyright ©2020 Peter Baumgartner. All rights reserved.
+//
+//**********************************************************************************************************************
+
+
+import SwiftUI
+import AppKit
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
+public extension NSSavePanel
+{
+	class func show(title:String? = nil, message:String? = nil, buttonLabel:String? = nil, defaultFilename:String? = nil, handler:(URL?)->Void)
+	{
+		let panel = NSSavePanel()
+		
+		panel.canCreateDirectories = true
+		
+		if let title = title
+		{
+			panel.title = title
+		}
+		
+		if let message = message
+		{
+			panel.message = message
+		}
+		
+		if let buttonLabel = buttonLabel
+		{
+			panel.prompt = buttonLabel
+		}
+		
+		if let defaultFilename = defaultFilename
+		{
+			panel.nameFieldStringValue = defaultFilename
+		}
+		
+		let button = panel.runModal()
+		
+		if button == NSApplication.ModalResponse.OK
+		{
+			handler(panel.url)
+		}
+		else
+		{
+			handler(nil)
+		}
+	}
+}
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
