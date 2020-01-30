e944a4e798e45f392511c36dbb602e7d9f6fdd85
Peter Baumgartner <peter@boinx.com>
2020-01-30 16:02:34 +0100

    Added documentation comments

--- BXSwiftUI/Binding+Optional.swift
+++ BXSwiftUI/Binding+Optional.swift
@@ -15,6 +15,12 @@ import SwiftUI
 
 extension Binding
 {
+	/// Creates a non-optional Binding to an optional property T? of an object.
+	/// - parameter object: The object that contains the optional property
+	/// - parameter keyPath: The keypath to the optional property
+	/// - parameter defaultValue: The value to be used if the property is nil
+	/// - returns: A non-optional Binding of type T
+
 	public init<T:AnyObject>(object:T, keyPath:ReferenceWritableKeyPath<T,Value?>, defaultValue:Value)
 	{
 		self.init(
