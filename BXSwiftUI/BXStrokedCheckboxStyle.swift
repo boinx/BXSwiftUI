2640a9e80bbff656c953844ed73e0779bec3529f
Peter Baumgartner <peter@boinx.com>
2020-02-03 10:42:52 +0100

    Bugfix: BXStrokedCheckboxStyle now respects environment values

--- BXSwiftUI/BXStrokedCheckboxStyle.swift
+++ BXSwiftUI/BXStrokedCheckboxStyle.swift
@@ -15,18 +15,38 @@ import SwiftUI
 
 public struct BXStrokedCheckboxStyle : ToggleStyle
 {
+	public init() {}
+    
+	// Unfortunately @Environment values cannot be accessed directly from a ToggleStyle, so
+	// we will create a private subview, which does have access to the @Environment values.
+	
+    public func makeBody(configuration: Self.Configuration) -> some View
+    {
+		_BXStrokedCheckbox(configuration:configuration)
+    }
+}
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
+// Internal view type that does the actual rendering on behalf of the BXStrokedCheckboxStyle.
+// Since it is a View, it does have access to @Environment values!
+
+fileprivate struct _BXStrokedCheckbox : View
+{
+	// Params
+	
+	let configuration:ToggleStyle.Configuration
+	
 	// Environment
 	
 	@Environment(\.isEnabled) private var isEnabled
 	@Environment(\.controlSize) private var controlSize
 	@Environment(\.hasMultipleValues) private var hasMultipleValues
+
+	// Sizing depends on environment controlSize
 	
-	// Necessary to make init available outside this module
-	
-	public init() {}
-    
-    // Edge length and corner radius of the checkbox depend on environment controlSize
-    
 	private var edge:CGFloat
 	{
 		switch controlSize
@@ -51,65 +71,77 @@ public struct BXStrokedCheckboxStyle : ToggleStyle
 		}
 	}
 
-	// Draw the checkbox
+	private var offset:CGFloat
+	{
+		switch controlSize
+		{
+			case .regular: 		return 0.5
+			case .small: 		return 0.0
+			case .mini: 		return 0.0
+			
+			@unknown default:	return 0.5
+		}
+	}
+
+	// Build the view
 	
-    public func makeBody(configuration: Self.Configuration) -> some View
-    {
+	var body: some View
+	{
 		let fillColor = configuration.isOn || self.hasMultipleValues ?
 			Color.accentColor :
 			Color(white:1.0, opacity:0.07)
 		
-        return HStack
-        {
-            ZStack()
-            {
+		return HStack
+		{
+			ZStack()
+			{
 				// Fill
 				
-                RoundedRectangle(cornerRadius:radius)
-                    .foregroundColor(fillColor)
-                    .frame(width:edge, height:edge)
+				RoundedRectangle(cornerRadius:radius)
+					.foregroundColor(fillColor)
+					.frame(width:edge, height:edge)
 
 				// Stroke
 				
 				RoundedRectangle(cornerRadius:radius)
-                    .stroke(Color.gray, lineWidth:0.5)
+					.stroke(Color.gray, lineWidth:0.5)
 					.frame(width:edge, height:edge)
 				
 				// Content
 				
 				if self.hasMultipleValues
 				{
-					Text("–").bold().offset(x:0.5, y:0)
+					Image(systemName:"minus")
 				}
 				else if configuration.isOn
 				{
-					Text("✓").bold().offset(x:0.5, y:0)
+					Text("✓").bold().offset(x:offset, y:0)
 				}
-            }
+			}
 			
 			// Label
 			
 			configuration.label
-        }
-        
-        // Dim when disabled
-        
-        .opacity(self.isEnabled ? 1.0 : 0.33)
-        
-        // Event handling
-        
+		}
+		
+		// Dim when disabled
+		
+		.opacity(self.isEnabled ? 1.0 : 0.33)
+		
+		// Event handling
+		
 		.onTapGesture
 		{
 			if self.hasMultipleValues
 			{
-				configuration.$isOn.wrappedValue = true
+				self.configuration.$isOn.wrappedValue = true
 			}
 			else
 			{
-				configuration.$isOn.wrappedValue.toggle()
+				self.configuration.$isOn.wrappedValue.toggle()
 			}
 		}
-    }
+	}
 }
 
 
