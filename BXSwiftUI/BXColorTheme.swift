f4bf7fbeddca37b7e3f0f88c45a5d559d18ed07d
Peter Baumgartner <peter@boinx.com>
2020-02-10 08:48:45 +0100

    Improved public API for BXColorTheme

--- BXSwiftUI/BXColorTheme.swift
+++ BXSwiftUI/BXColorTheme.swift
@@ -15,9 +15,31 @@ import SwiftUI
 
 public struct BXColorTheme
 {
-	public let fillColor:		(ColorScheme,Bool,Double) -> Color
-	public let strokeColor:		(ColorScheme,Bool,Double) -> Color
-	public let contentColor:	(ColorScheme,Bool,Double) -> Color
+	internal let fillColor:		(ColorScheme,Bool,Double) -> Color
+	internal let strokeColor:	(ColorScheme,Bool,Double) -> Color
+	internal let contentColor:	(ColorScheme,Bool,Double) -> Color
+}
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
+extension BXColorTheme
+{
+	public func fillColor(for colorScheme:ColorScheme = .dark, isEnabled:Bool = true, enhanceBy factor:Double = 1.0) -> Color
+	{
+		return self.fillColor(colorScheme,isEnabled,factor)
+	}
+	
+	public func strokeColor(for colorScheme:ColorScheme = .dark, isEnabled:Bool = true, enhanceBy factor:Double = 1.0) -> Color
+	{
+		return self.strokeColor(colorScheme,isEnabled,factor)
+	}
+	
+	public func contentColor(for colorScheme:ColorScheme = .dark, isEnabled:Bool = true, enhanceBy factor:Double = 1.0) -> Color
+	{
+		return self.contentColor(colorScheme,isEnabled,factor)
+	}
 }
 
 
@@ -35,21 +57,21 @@ extension BXColorTheme
 		)
 	}
 	
-	public static func defaultFillColor(for colorScheme:ColorScheme, isEnabled:Bool, enhanceBy factor:Double = 1.0) -> Color
+	internal static func defaultFillColor(for colorScheme:ColorScheme, isEnabled:Bool, enhanceBy factor:Double = 1.0) -> Color
 	{
 		let gray = colorScheme == .dark ? 1.0 : 0.0
 		let alpha = isEnabled ? 0.1*factor : 0.033*factor
 		return Color(white:gray, opacity:alpha)
 	}
 
-	public static func defaultStrokeColor(for colorScheme:ColorScheme, isEnabled:Bool, enhanceBy factor:Double = 1.0) -> Color
+	internal static func defaultStrokeColor(for colorScheme:ColorScheme, isEnabled:Bool, enhanceBy factor:Double = 1.0) -> Color
 	{
 		let gray = colorScheme == .dark ? 0.65*factor : 0.35/factor
 		let alpha = isEnabled ? 1.0 : 0.33
 		return Color(white:gray, opacity:alpha)
 	}
 	
-	public static func defaultContentColor(for colorScheme:ColorScheme, isEnabled:Bool, enhanceBy factor:Double = 1.0) -> Color
+	internal static func defaultContentColor(for colorScheme:ColorScheme, isEnabled:Bool, enhanceBy factor:Double = 1.0) -> Color
 	{
 		let gray = colorScheme == .dark ? 1.0*factor : 1.0-1.0*factor
 		let alpha = isEnabled ? 1.0 : 0.33
