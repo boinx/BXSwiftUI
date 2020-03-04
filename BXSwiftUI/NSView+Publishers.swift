edc58e39b3d87f0d076d3e670445e0c96c422ded
Peter Baumgartner <peter@boinx.com>
2020-03-04 09:16:48 +0100

    Added NSView frame/boundsDidChange Publishers

---
+++ BXSwiftUI/NSView+Publishers.swift
@@ -0,0 +1,45 @@
+//**********************************************************************************************************************
+//
+//  NSView+Publishers.swift
+//	Publishers for view frame/boundsDidChange notifications
+//  Copyright ©2020 Peter Baumgartner. All rights reserved.
+//
+//**********************************************************************************************************************
+
+
+import AppKit
+import Combine
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
+public extension NSView
+{
+	/// Returns a publisher for frame changes of a NSView
+	
+	var frameDidChangePublisher : AnyPublisher<NSRect,Never>
+	{
+		self.postsFrameChangedNotifications = true
+		
+		return NotificationCenter.default
+			.publisher(for:NSView.frameDidChangeNotification, object:self)
+			.compactMap { ($0.object as? NSView)?.frame }
+			.eraseToAnyPublisher()
+	}
+
+	/// Returns a publisher for bounds changes of a NSView
+	
+	var boundsDidChangePublisher : AnyPublisher<NSRect,Never>
+	{
+		self.postsBoundsChangedNotifications = true
+		
+		return NotificationCenter.default
+			.publisher(for:NSView.boundsDidChangeNotification, object:self)
+			.compactMap { ($0.object as? NSView)?.bounds }
+			.eraseToAnyPublisher()
+	}
+}
+
+
+//----------------------------------------------------------------------------------------------------------------------
