f26b531351e64b8df3cb558968dfd04e9c8bdc2b
Peter Baumgartner <peter@boinx.com>
2020-03-13 15:16:02 +0100

    Renamed file

--- BXSwiftUI/View+descriptionLabel.swift
+++ BXSwiftUI/View+descriptionLabel.swift
@@ -1,6 +1,6 @@
 //**********************************************************************************************************************
 //
-//  View+description.swift
+//  View+descriptionLabel.swift
 //	Adds a descriptive label below a View
 //  Copyright ©2020 Peter Baumgartner. All rights reserved.
 //
@@ -18,15 +18,16 @@ public extension View
 	/// Adds a descriptive label below a View. This label will be dimmed, so that the original View appears
 	/// to be more dominant (important).
 	
-	func description(_ description:String) -> some View
+	func descriptionLabel(_ description:String) -> some View
 	{
 		VStack(alignment:.leading)
 		{
 			self
 
 			Text(description)
-				.lineLimit(nil)
 				.controlSize(.small)
+				.lineLimit(nil)									// Wrap text to as many lines as needed
+				.fixedSize(horizontal:false, vertical:true) 	// Workaround because .lineLimit(nil) doesn't work by itself
 				.opacity(0.33)
 		}
 	}
