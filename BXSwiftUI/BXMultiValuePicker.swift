019e8ffa8ec486e02375417aa7a0982b300deaff
Peter Baumgartner <peter@boinx.com>
2020-03-13 10:17:16 +0100

    Added single value Picker

--- BXSwiftUI/BXMultiValuePicker.swift
+++ BXSwiftUI/BXMultiValuePicker.swift
@@ -15,6 +15,45 @@ import AppKit
 //----------------------------------------------------------------------------------------------------------------------
 
 
+public struct BXPicker : View
+{
+	// Params
+	
+	private var value:Binding<Int>
+	private var initialAction:(()->Void)? = nil
+	private var orderedItems:[BXMenuItemSpec]
+
+	// Init
+	
+	public init(value:Binding<Int>, initialAction:(()->Void)? = nil, orderedItems:[BXMenuItemSpec])
+	{
+		self.value = value
+		self.initialAction = initialAction
+		self.orderedItems = orderedItems
+	}
+	
+	// Build View
+	
+	public var body: some View
+	{
+		BXMultiValuePicker(values:multiValueBinding, initialAction:initialAction, orderedItems:orderedItems)
+	}
+
+	// Convert between Int and Set<Int>
+	
+	private var multiValueBinding:Binding<Set<Int>>
+	{
+		return Binding<Set<Int>>(
+			get:{ Set([self.value.wrappedValue]) },
+			set:{ self.value.wrappedValue = $0.first ?? 0 }
+		)
+	}
+}
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
 public struct BXMultiValuePicker : NSViewRepresentable
 {
 	// Params
