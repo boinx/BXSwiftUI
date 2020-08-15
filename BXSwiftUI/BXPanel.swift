8fe70cf8477d3d207a5113058eb9c6988fec91f2
Peter Baumgartner <peter@boinx.com>
2020-08-15 09:29:28 +0200

    Improved BXPanel window positioning

--- BXSwiftUI/BXPanel.swift
+++ BXSwiftUI/BXPanel.swift
@@ -15,12 +15,23 @@ import BXUIKit
 //----------------------------------------------------------------------------------------------------------------------
 
 
-public class BXPanel : NSPanel
+public class BXPanel : NSPanel, NSWindowDelegate
 {
+	/// Store the number of BXPanels that are currently open
+	
+	private static var panelCount = 0
+
+	/// Stores the topLeft corner position of the last BXPanel that was opened
+	
+	private static var topLeft:CGPoint = .zero
+	
+	
+//----------------------------------------------------------------------------------------------------------------------
+
 
     /// Creates an NSPanel with the specified SwiftUI view as contents
     
-    public init<V:View>(with view:V, title:String? = nil)
+    public init<V:View>(with view:V, title:String? = nil, shouldCascade:Bool = true)
     {
 		// Create a hud panel window
 	
@@ -30,7 +41,10 @@ public class BXPanel : NSPanel
 			backing:.buffered,
 			defer:false)
 
+		self.delegate = self
 		self.isMovableByWindowBackground = true
+
+		Self.panelCount += 1
 
 		// Set optional title
 		
@@ -54,10 +68,33 @@ public class BXPanel : NSPanel
 		hostingView.bottomAnchor.constraint(equalTo:rootView.bottomAnchor).isActive = true
 		hostingView.leadingAnchor.constraint(equalTo:rootView.leadingAnchor).isActive = true
 		hostingView.trailingAnchor.constraint(equalTo:rootView.trailingAnchor).isActive = true
+		
+		// Position window
+		
+		self.center()
+		
+		if shouldCascade
+		{
+			Self.topLeft = self.cascadeTopLeft(from:Self.topLeft)
+		}
     }
+	
 
+//----------------------------------------------------------------------------------------------------------------------
+
+
+	// After the last BXPanel is closed reset the window position to default (center)
+	
+	public func windowWillClose(_ notification: Notification)
+    {
+		Self.panelCount -= 1
+		
+		if Self.panelCount == 0
+		{
+			Self.topLeft = .zero
+		}
+    }
 }
 
 
 //----------------------------------------------------------------------------------------------------------------------
-
