8e8122ceac27dfde22579255cb3c1067ff2070b7
Peter Baumgartner <peter@boinx.com>
2020-09-23 15:44:36 +0200

    Added hiliteColor to BXColorTheme

--- BXSwiftUI/BXColorTheme.swift
+++ BXSwiftUI/BXColorTheme.swift
@@ -19,13 +19,15 @@ public struct BXColorTheme
 	let fillColor: (ColorScheme,Bool,Double)->Color
 	let strokeColor: (ColorScheme,Bool,Double)->Color
 	let contentColor: (ColorScheme,Bool,Double)->Color
+	let hiliteColor: (ColorScheme,Bool,Double)->Color
 	
-	public init(backgroundColor:@escaping (ColorScheme)->Color, fillColor:@escaping (ColorScheme,Bool,Double)->Color, strokeColor:@escaping (ColorScheme,Bool,Double)->Color, contentColor:@escaping (ColorScheme,Bool,Double)->Color)
+	public init(backgroundColor:@escaping (ColorScheme)->Color, fillColor:@escaping (ColorScheme,Bool,Double)->Color, strokeColor:@escaping (ColorScheme,Bool,Double)->Color, contentColor:@escaping (ColorScheme,Bool,Double)->Color, hiliteColor:@escaping (ColorScheme,Bool,Double)->Color)
 	{
 		self.backgroundColor = backgroundColor
 		self.fillColor = fillColor
 		self.strokeColor = strokeColor
 		self.contentColor = contentColor
+		self.hiliteColor = hiliteColor
 	}
 }
 
@@ -54,6 +56,11 @@ extension BXColorTheme
 	{
 		return self.contentColor(colorScheme,isEnabled,factor)
 	}
+	
+	public func hiliteColor(for colorScheme:ColorScheme = .dark, isEnabled:Bool = true, enhanceBy factor:Double = 1.0) -> Color
+	{
+		return self.hiliteColor(colorScheme,isEnabled,factor)
+	}
 }
 
 
@@ -68,17 +75,18 @@ extension BXColorTheme
 			backgroundColor:	self.defaultBackgroundColor,
 			fillColor:			self.defaultFillColor,
 			strokeColor:		self.defaultStrokeColor,
-			contentColor:		self.defaultContentColor
+			contentColor:		self.defaultContentColor,
+			hiliteColor:		self.defaultHiliteColor
 		)
 	}
 	
-	internal static func defaultBackgroundColor(for colorScheme:ColorScheme) -> Color
+	public static func defaultBackgroundColor(for colorScheme:ColorScheme) -> Color
 	{
 		let gray = colorScheme == .dark ? 0.19 : 0.85
 		return Color(white:gray, opacity:1.0)
 	}
 
-	internal static func defaultFillColor(for colorScheme:ColorScheme, isEnabled:Bool, enhanceBy factor:Double = 1.0) -> Color
+	public static func defaultFillColor(for colorScheme:ColorScheme, isEnabled:Bool, enhanceBy factor:Double = 1.0) -> Color
 	{
 //		let gray = colorScheme == .dark ? 1.0 : 0.0
 //		let alpha = isEnabled ? 0.1*factor : 0.033*factor
@@ -94,18 +102,24 @@ extension BXColorTheme
 		return Color(white:1.0, opacity:alpha)
 	}
 
-	internal static func defaultStrokeColor(for colorScheme:ColorScheme, isEnabled:Bool, enhanceBy factor:Double = 1.0) -> Color
+	public static func defaultStrokeColor(for colorScheme:ColorScheme, isEnabled:Bool, enhanceBy factor:Double = 1.0) -> Color
 	{
 		let gray = colorScheme == .dark ? 0.65*factor : 0.6/factor
 		let alpha = isEnabled ? 1.0 : 0.33
 		return Color(white:gray, opacity:alpha)
 	}
 	
-	internal static func defaultContentColor(for colorScheme:ColorScheme, isEnabled:Bool, enhanceBy factor:Double = 1.0) -> Color
+	public static func defaultContentColor(for colorScheme:ColorScheme, isEnabled:Bool, enhanceBy factor:Double = 1.0) -> Color
 	{
 		let gray = colorScheme == .dark ? 1.0*factor : 0.1/factor
 		let alpha = isEnabled ? 1.0 : 0.33
 		return Color(white:gray, opacity:alpha)
+	}
+	
+	public static func defaultHiliteColor(for colorScheme:ColorScheme, isEnabled:Bool, enhanceBy factor:Double = 1.0) -> Color
+	{
+		let alpha = isEnabled ? 1.0 : 0.33
+		return Color.accentColor.opacity(alpha)
 	}
 }
 
