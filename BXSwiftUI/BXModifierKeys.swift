b899c4ea0840adb296d3dc071a367f6918761f45
Peter Baumgartner <peter@boinx.com>
2021-01-26 11:15:26 +0100

    Added detection of regular key presses (without modifiers)

--- BXSwiftUI/BXModifierKeys.swift
+++ BXSwiftUI/BXModifierKeys.swift
@@ -20,6 +20,10 @@ public final class BXModifierKeys : NSObject, ObservableObject
 	
 	public static var shared = BXModifierKeys()
 	
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
 	/// The current set of pressed modifier keys
 	
 	public var flags:NSEvent.ModifierFlags
@@ -33,6 +37,30 @@ public final class BXModifierKeys : NSObject, ObservableObject
     public func flagsChanged(with event:NSEvent)
     {
 		self.objectWillChange.send()
+    }
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
+	/// The currently pressed key
+	
+	@Published public var key:String? = nil
+	
+	/// Call this function from a keyDown() somewhere in your responder chain. The best place would be near the
+	/// root (e.g. in the AppDelegate). Make sure to call super so that the responder chain doesn't get broken.
+	
+    public func keyDown(with event:NSEvent)
+    {
+		self.key = event.charactersIgnoringModifiers
+    }
+    
+	/// Call this function from a keyUp() somewhere in your responder chain. The best place would be near the
+	/// root (e.g. in the AppDelegate). Make sure to call super so that the responder chain doesn't get broken.
+	
+    public func keyUp(with event:NSEvent)
+    {
+		self.key = nil
     }
 }
 
