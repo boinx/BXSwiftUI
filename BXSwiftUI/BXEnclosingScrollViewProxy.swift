b5438e1d01e5c23a70d3c615ba269c3444b1474d
Peter Baumgartner <peter@boinx.com>
2021-02-18 10:35:27 +0100

    Enhanced BXEnclosingScrollViewProxy to preserve scroll position when content size changes

--- BXSwiftUI/BXEnclosingScrollViewProxy.swift
+++ BXSwiftUI/BXEnclosingScrollViewProxy.swift
@@ -2,29 +2,41 @@
 //
 //  BXEnclosingScrollViewProxy.swift
 //	A helper view that helps with lazy loading of child views
-//  Copyright ©2020 Peter Baumgartner. All rights reserved.
+//  Copyright ©2020-2021 Peter Baumgartner. All rights reserved.
 //
 //**********************************************************************************************************************
 
 
 import SwiftUI
 import AppKit
+import BXSwiftUtils
 
 
 //----------------------------------------------------------------------------------------------------------------------
 
 
-/// BXEnclosingScrollViewProxy calls its content builder closure with the visibleRect of the enclosing ScrollView. This
+/// BXEnclosingScrollViewProxy calls its content builder closure with the visibleRect of the enclosing ScrollView. This can be used to lazily build only those subviews that are actually going to be visible, which is a huge performance win if there are hundreds or even thousands of subviews.
 
 public struct BXEnclosingScrollViewProxy<Content:View> : View
 {
-	private var content:(CGRect)->Content
+	/// Set to true if you want to preserve the scroll position whenever the content size changes.
+	
+	private var preserveScrollPos:Bool = false
+	
+	/// The content for the scrollview
+	
+	private var content:(NSScrollView?,CGRect)->Content
+	
+	// State
+	
+	@State private var scrollView:NSScrollView? = nil
 	@State private var visibleRect = CGRect.zero
 
 	// Init
 	
-	public init(@ViewBuilder content:@escaping (CGRect)->Content)
+	public init(preserveScrollPos:Bool = true, @ViewBuilder content:@escaping (NSScrollView?,CGRect)->Content)
 	{
+		self.preserveScrollPos = preserveScrollPos
 		self.content = content
 	}
 
@@ -36,13 +48,13 @@ public struct BXEnclosingScrollViewProxy<Content:View> : View
 		// created and which children should be skipped because they wouldn't be visible anyways.
 		// This can improve performance.
 		
-		self.content(visibleRect)
+		self.content(scrollView,visibleRect)
 		
 			// Attach a _BXEnclosingScrollViewProxy to track the visibleRect of an enclosingScrollView,
 			// i.e. an AppKit NSScrollView
 			
 			.background(
-				_BXEnclosingScrollViewProxy(visibleRect:$visibleRect)
+				_BXEnclosingScrollViewProxy(scrollView:$scrollView, visibleRect:$visibleRect, preserveScrollPos:preserveScrollPos)
 			)
     }
 }
@@ -55,9 +67,15 @@ public struct BXEnclosingScrollViewProxy<Content:View> : View
 
 fileprivate struct _BXEnclosingScrollViewProxy : NSViewRepresentable
 {
-	/// A binding to the @State from above
+	// Params
 	
+	var scrollView:Binding<NSScrollView?>
 	var visibleRect:Binding<CGRect>
+    var preserveScrollPos:Bool = true
+    
+    // State
+    
+    @State private var savedScrollPos:CGPoint? = nil
     
     // Build View
     
@@ -65,10 +83,23 @@ fileprivate struct _BXEnclosingScrollViewProxy : NSViewRepresentable
     {
 		let view = _TrackingView(frame:.zero)
 		
-		view.didScrollHandler =
+		view.scrollView = self.scrollView
+		
+		view.didChangeSize =
+		{
+			size in self.saveScrollPos()
+		}
+		
+		view.didScroll =
 		{
 			visibleRect in
-			DispatchQueue.main.async { self.visibleRect.wrappedValue = visibleRect } // Must be deferred to avoid SwiftUI state modification issues during body!
+			
+			self.restoreScrollPos()
+
+			DispatchQueue.main.async
+			{
+				self.visibleRect.wrappedValue = visibleRect // Must be deferred to avoid SwiftUI state modification issues during body!
+			}
 		}
 		
 		return view
@@ -77,6 +108,23 @@ fileprivate struct _BXEnclosingScrollViewProxy : NSViewRepresentable
 	func updateNSView(_ view:_TrackingView, context:Context)
     {
 		
+	}
+	
+	func saveScrollPos()
+	{
+		guard self.preserveScrollPos else { return }
+		guard let scrollView = self.scrollView.wrappedValue else { return }
+		self.savedScrollPos = scrollView.contentView.bounds.origin
+	}
+	
+	func restoreScrollPos()
+	{
+		guard self.preserveScrollPos else { return }
+		guard let scrollView = self.scrollView.wrappedValue else { return }
+		guard let savedScrollPos = self.savedScrollPos else { return }
+		
+		scrollView.documentView?.scroll(savedScrollPos)
+		self.savedScrollPos = nil
 	}
  }
 
@@ -88,13 +136,20 @@ fileprivate struct _BXEnclosingScrollViewProxy : NSViewRepresentable
 
 fileprivate class _TrackingView : NSView
 {
-	/// The visibleRec tof the enclosingScrollView is reported by the didScrollHandler closure
+
+	var scrollView:Binding<NSScrollView?>? = nil
+
+	/// Called with the visibleRect whenever the view is scrolled
 	
-	var didScrollHandler:((CGRect)->Void)? = nil
+	var didScroll:((CGRect)->Void)? = nil
+	
+	/// Called when the size of the content changes
+	
+	var didChangeSize:((CGSize)->Void)? = nil
 	
 	/// Retains the NotificationCenter observer
 	
-	var observer:Any? = nil
+	var observers:[Any] = []
 	
 	/// Performs setup when the helper view is added to the view hierarchy
 	
@@ -106,22 +161,37 @@ fileprivate class _TrackingView : NSView
 		// Find the enclosing NSScrollView and its NSClipView
 		
 		guard let scrollView = self.enclosingScrollView else { return }
+		guard let documentView = scrollView.documentView else { return }
 		let clipView = scrollView.contentView
-		
-		// Call the didScrollHandler with the initial visibleRect
-		
-		self.didScrollHandler?(clipView.bounds)
 
-		// Whenever the view is scrolled, call the didScrollHandler with the new visibleRect
+		// Report initial values
+		
+		self.scrollView?.wrappedValue = scrollView
+		self.didChangeSize?(documentView.bounds.size)
+		self.didScroll?(clipView.bounds)
+		
+		// Whenever the content changes, report it new size
+		
+		documentView.postsFrameChangedNotifications = true
+
+		self.observers += NotificationCenter.default.addAutoRemovingObserver(forName:NSView.frameDidChangeNotification, object:documentView, queue:.main)
+		{
+			[weak self,weak documentView] _ in
+			guard let self = self else { return }
+			guard let documentView = documentView else { return }
+			self.didChangeSize?(documentView.bounds.size)
+		}
+		
+		// Whenever the view is scrolled, report the new visibleRect
 		
 		clipView.postsBoundsChangedNotifications = true
 
-		self.observer = NotificationCenter.default.addAutoRemovingObserver(forName:NSView.boundsDidChangeNotification, object:clipView, queue: OperationQueue.main)
+		self.observers += NotificationCenter.default.addAutoRemovingObserver(forName:NSView.boundsDidChangeNotification, object:clipView, queue:.main)
 		{
 			[weak self,weak clipView] _ in
 			guard let self = self else { return }
 			guard let clipView = clipView else { return }
-			self.didScrollHandler?(clipView.bounds)
+			self.didScroll?(clipView.bounds)
 		}
 	}
 }
