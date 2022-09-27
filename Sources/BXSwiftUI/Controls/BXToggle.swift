31529ad2993c2c2369056e1ddf82743448a4895d
Peter Baumgartner <peter@boinx.com>
2022-09-27 16:27:20 +0200

    Added BXToggle (for single value bindings)

---
+++ Sources/BXSwiftUI/Controls/BXToggle.swift
@@ -0,0 +1,81 @@
+//**********************************************************************************************************************
+//
+//  BXToggle.swift
+//	SwiftUI wrapper for a checkbox
+//  Copyright ©2020 Peter Baumgartner. All rights reserved.
+//
+//**********************************************************************************************************************
+
+
+#if os(macOS)
+
+import SwiftUI
+import AppKit
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
+public struct BXToggle : View
+{
+	// Params
+	
+	private var value:Binding<Bool>
+	private var label:String = ""
+	private var onBegan:(()->Void)? = nil
+	private var onEnded:(()->Void)? = nil
+	
+	// Environment
+	
+	@Environment(\.isEnabled) private var isEnabled
+	@Environment(\.controlSize) private var controlSize
+	@Environment(\.bxUndoManagerProvider) private var undoManagerProvider
+	@Environment(\.bxUndoName) private var undoName
+
+	// Init
+	
+	public init(value:Binding<Bool>, label:String = "", onBegan:(()->Void)? = nil, onEnded:(()->Void)? = nil)
+	{
+		self.value = value
+		self.label = label
+		self.onBegan = onBegan
+		self.onEnded = onEnded
+	}
+	
+	// Build View
+	
+	public var body: some View
+	{
+		Toggle(isOn:self.valueBinding)
+		{
+			Text(label)
+		}
+	}
+
+	// Custom Binding
+	
+	private var valueBinding:Binding<Bool>
+	{
+		 Binding<Bool>(
+		 
+			get:
+			{
+				self.value.wrappedValue
+			},
+			
+			set:
+			{
+				self.onBegan?()
+				self.value.wrappedValue = $0
+				self.onEnded?()
+				self.undoManagerProvider.undoManager?.setActionName(self.undoName)
+			})
+	}
+
+}
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
+#endif
