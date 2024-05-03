6064018b97498ab0bd31f8a5ac5ef400a45e85f7
Peter Baumgartner <peter@boinx.com>
2024-05-03 10:29:52 +0200

    Refactoring: Moved common code to BXSwiftUtils

--- Sources/BXSwiftUI/Misc/BXMenuItemSpec.swift
+++ Sources/BXSwiftUI/Misc/BXMenuItemSpec.swift
@@ -8,6 +8,7 @@
 
 
 import SwiftUI
+import BXSwiftUtils
 
 #if canImport(AppKit)
 import AppKit
@@ -183,39 +184,4 @@ public extension NSMenu
 //----------------------------------------------------------------------------------------------------------------------
 
 
-extension NSAttributedString
-{
-	/// Creates an NSAttributedString with the specified string. It uses the regular system font, but any occurances of "[AI]" will be styled in a special way.
-	
-	public convenience init(with string:String)
-	{
-		let regularFont = NSFont.systemFont(ofSize:NSFont.systemFontSize)
-		let regularAttrs = [NSAttributedString.Key.font:regularFont]
-
-		let smallFont = NSFont.systemFont(ofSize:9)
-		let smallAttrs:[NSAttributedString.Key:Any] = [NSAttributedString.Key.font:smallFont, NSAttributedString.Key.foregroundColor:NSColor.systemYellow, NSAttributedString.Key.baselineOffset:3]
-		
-		let parts = string.components(separatedBy:"[AI]")
-		
-		let text = NSMutableAttributedString()
-		
-		for (i,part) in parts.enumerated()
-		{
-			text.append(NSMutableAttributedString(string:part, attributes:regularAttrs))
-			
-			if i < parts.count-1
-			{
-				let AI = NSLocalizedString("AI", tableName:"BXMenuItemSpec", bundle:.BXSwiftUI, comment:"AI abbreviation")
-				text.append(NSMutableAttributedString(string:AI, attributes:smallAttrs))
-			}
-		}
-		
-		self.init(attributedString:text)
-	}
-}
-
 #endif
-
-
-//----------------------------------------------------------------------------------------------------------------------
-
